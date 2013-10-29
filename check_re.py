3cfbe777349c1dd5a0d3529235282dde2998b946
Leo Liu <changtong1993@gmail.com>
2013-10-29 23:13:54 +0800

    ...

--- check_re.py
+++ check_re.py
@@ -1,1 +1,13 @@
 import re
+
+class CheckRegex(object):
+    """ check some common format """
+
+    def checkUserName(self, userName):
+        pass
+
+    def checkMail(self, mail):
+        pass
+
+    def checkPhoneNumber(self, phoneNumber):
+        pass

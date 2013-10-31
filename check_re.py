abba61e20004a7ed24aedd5629241e7a7935b9d1
Leo Liu <changtong1993@gmail.com>
2013-10-31 20:26:20 +0800

    为 check_re.py 添加部分功能

--- check_re.py
+++ check_re.py
@@ -1,13 +1,48 @@
+#coding=utf8
 import re
 
 class CheckRegex(object):
-    """ check some common format """
+    """ Regular expressions to check some common formats """
 
     def checkUserName(self, userName):
-        pass
+        ''' 字母开头，允许5-16字节，允许字母数字下划线 '''
+        if re.match("", userName) != None:
+            return True
+        else:
+            return False
 
-    def checkMail(self, mail):
-        pass
+    def checkMail(self, mailAddress):
+        if re.match("^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", mailAddress) != None:
+            return True
+        else:
+            return False
 
     def checkPhoneNumber(self, phoneNumber):
-        pass
+        if re.match("^\d{3}-\d{8}|\d{4}-\d{7,8}$", phoneNumber) != None:
+            return True
+        else:
+            return False
+
+    def checkMobilePhoneNumber(self, phoneNumber):
+        if re.match("^[1-9][3,5,8]\d{9}$", phoneNumber) != None:
+            return True
+        else:
+            return False
+
+    def checkURL(self, url):
+        if re.match("[a-zA-Z]+://[^\s]*", url) != None:
+            return True
+        else:
+            return False
+
+    def checkQQ(self, qqNumber):
+        if re.match("[1-9][0-9]{4,9}", qqNumber) != None:
+            return True
+        else:
+            return False
+
+    def checkIP(self, ipAddress):
+        if re.match("^\d+\.\d+\.\d+\.\d+$"):
+            return True
+        else:
+            return False
